7005621d8c15617ce32b6209192dba806479b2f0
hamo <hamo@email.ru>
2023-04-23 11:20:08 +0400

    release 0.1.1

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider, MobileDatePicker } from "@mui/x-date-pickers";
+import { Button } from "@mui/material";
 import "./style.scss";
 
 function TodoList() {
-    return <div>TodoList</div>
+    return <div className="todo_list_wrapper">
+        <h3 className="todo_list_title">To do List</h3>
+        <div className="new_task_wrapper">
+            <h3 className="new_task_title">New Task</h3>
+            <div className="new_task_inputs_wrapper">
+            <TextField variant="outlined" />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <MobileDatePicker defaultValue={dayjs("2022-04-17")} />
+            </LocalizationProvider>
+            <Button variant="contained">Add</Button>
+            </div>
+        </div>
+    </div>
 }
 
 export default TodoList;
