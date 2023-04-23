5249559d73be7af2f4fb4b256b7463be95314129
hamo <hamo@email.ru>
2023-04-23 09:26:02 +0400

    hamo

--- src/components/Y/actions/index.js
+++
@@ -1,8 +0,0 @@
-import { state } from "..";
-
-export function changeTextInputValue(e) {
-    state.firstInput = e.target.value
-}
-export function changeDateInputValue(e) {
-    state.secondInput = e.target.value
-}
