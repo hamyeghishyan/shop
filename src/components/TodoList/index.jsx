3c44554e513e82840c0ac8ec3a51452c9ee31f8c
hamo_dev <hamoyeghishyan@gmail.com>
2023-04-25 00:31:03 +0400

    create functions

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -4,20 +4,40 @@ import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider, MobileDatePicker } from "@mui/x-date-pickers";
 import { Button } from "@mui/material";
+import { useState } from "react";
 import "./style.scss";
 
 function TodoList() {
+
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
+
+    function addItem() {
+        console.log(newItem);
+    }
+
     return <div className="todo_list_wrapper">
         <h3 className="todo_list_title">To do List</h3>
         <div className="new_task_wrapper">
             <h3 className="new_task_title">New Task</h3>
             <div className="new_task_inputs_wrapper">
-            <TextField variant="outlined" />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <MobileDatePicker defaultValue={dayjs("2022-04-17")} />
+            <TextField variant="outlined" placeholder="Type here"
+            />
+            <LocalizationProvider 
+                dateAdapter={AdapterDayjs}
+                value = {newItem}
+                onChange={e => setNewItem(e.target.value)}
+            >
+                <MobileDatePicker defaultValue={dayjs("2022-04-17")} />
             </LocalizationProvider>
-            <Button variant="contained">Add</Button>
+            <Button variant="contained" onClick={() => addItem()} >Add</Button>
             </div>
+        </div>
+        <div className="date_wrapper">
+            <h3 className="dates_title">Dates</h3>
+            <p className="dates_paragraphs"></p>
+            <p className="dates_paragraphs"></p>
+            <p className="dates_paragraphs"></p>
         </div>
     </div>
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.scss';
-import { Provider } from 'react-redux';
 import store from './store';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
